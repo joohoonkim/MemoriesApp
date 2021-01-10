73f052345ee5f959bc610d14bf4a8e9320ddd58a
joohoonkim <joohoon.kim@outlook.com>
2021-01-10 11:08:23 -0500

    controller and model for posts

---
+++ app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public $table = 'posts';
+
+    public $fillable = ['slug','title','event_date','description','images'];
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PagesController::class, 'index']);
+
+Route::get('/main',[PostController::class, 'index']);
