7480c73d505768ccc2aad4ccc9514bc90407266d
joohoonkim <joohoon.kim@outlook.com>
2021-01-18 23:57:31 -0500

    login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,8 +9,16 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
-        <h1>
-            Login
-        </h1>
+        <div class="rounded content-main shadow-sm" style="margin-top:2rem; margin-bottom:2rem; padding:4rem;">
+        {!! Form::open(['action' => 'App\Http\Controllers\PagesController@authenticate', 'method' => 'POST']) !!}
+            <div class="form-group">
+                {{Form::text('username', "", ['placeholder'=>'Username','maxlength'=>30])}}
+            </div>
+            <div class="form-group">
+                {{Form::password('password', ['placeholder'=>'Password','maxlength'=>30])}}
+            </div>
+            {{Form::submit('Submit')}}
+        {!! Form::close() !!}
+        </div>
     </body>
 </html>
