1d8eb2f30739b833521c517317b9d79c7960fe86
joohoonkim <davidkim2552@gmail.com>
2021-02-04 20:03:27 -0500

    added csrf and requirement text

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -7,14 +7,21 @@
     </div>
     <div class="rounded content-main shadow-sm" style="margin-top:2rem; margin-bottom:2rem; padding:4rem;">
         {!! Form::open(['action' => 'App\Http\Controllers\PostController@store', 'enctype' => 'multipart/form-data', 'method' => 'POST']) !!}
+            @csrf
             <div class="form-group">
                 {{Form::label('title', 'Title')}}
                 {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Enter the title'])}}
             </div>
+            @if($errors->has('title'))
+                <p style="color:red;">{{ $errors->first('title') }}</p>
+            @endif
             <div class="form-group">
-                {{Form::label('body', 'Description')}}
-                {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Enter a description'])}}
+                {{Form::label('description', 'Description')}}
+                {{Form::textarea('description', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Enter a description'])}}
             </div>
+            @if($errors->has('description'))
+                <p style="color:red;">{{ $errors->first('description') }}</p>
+            @endif
             <div class="form-group">
                 {{ Form::date('event_date', date('Y-m-d', strtotime(date("Y-m-d")))) }}  
             </div>
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -7,20 +7,28 @@
     </div>
     <div class="rounded content-main shadow-sm" style="margin-top:2rem; margin-bottom:2rem; padding:4rem;">
         {!! Form::open(['action' => ['App\Http\Controllers\PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right', 'id' => 'delete-form']) !!}
+            @csrf
             @method('DELETE')
             {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
         {!! Form::close() !!}
         <br>
         {!! Form::open(['action' => ['App\Http\Controllers\PostController@update', $post->id], 'enctype' => 'multipart/form-data', 'method' => 'POST']) !!}
+            @csrf
             @method('PUT')
             <div class="form-group">
                 {{Form::label('title', 'Title')}}
                 {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Enter the title'])}}
             </div>
+            @if($errors->has('title'))
+                <p style="color:red;">{{ $errors->first('title') }}</p>
+            @endif
             <div class="form-group">
-                {{Form::label('body', 'Description')}}
-                {{Form::textarea('body', $post->description, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Enter a description'])}}
+                {{Form::label('description', 'Description')}}
+                {{Form::textarea('description', $post->description, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Enter a description'])}}
             </div>
+            @if($errors->has('description'))
+                <p style="color:red;">{{ $errors->first('description') }}</p>
+            @endif
             <div class="form-group">
                 {{ Form::date('event_date', date('Y-m-d', strtotime($post->event_date))) }}  
             </div>
